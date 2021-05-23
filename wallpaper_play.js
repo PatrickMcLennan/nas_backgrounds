c1f1242fc967ea2eb27a43fdc7ca895128cbce24
Patrick McLennan <patrick.a.mclennan@gmail.com>
2021-05-23 01:27:56 -0400

    prettier

--- wallpaper_play.js
+++ wallpaper_play.js
@@ -17,15 +17,41 @@ Promise.all([
     }),
     lib_1.getIgnoreList(path_1.default.resolve(__dirname, `ignore-list.txt`)),
     lib_1.readdir(path_1.default.resolve(__dirname)),
-]).then(([newResults, ignoreList, currentFiles]) => {
-    var _a, _b, _c, _d, _e, _f;
+])
+    .then(([newResults, ignoreList, currentFiles]) => {
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l;
     const resultsHtml = node_html_parser_1.parse(newResults.data);
-    const anchorElements = ;
-    const currentImages = (_b = (_a = currentFiles === null || currentFiles === void 0 ? void 0 : currentFiles.reduce) === null || _a === void 0 ? void 0 : _a.call(currentFiles, (all, current) => {
+    const imageResults = (_e = (_d = (_c = Array.from((_b = (_a = resultsHtml === null || resultsHtml === void 0 ? void 0 : resultsHtml.querySelectorAll) === null || _a === void 0 ? void 0 : _a.call(resultsHtml, `[data-download][data-src]`)) !== null && _b !== void 0 ? _b : [], (element) => {
+        var _a, _b, _c, _d;
+        return ({
+            title: lib_1.nameImage((_b = (_a = element === null || element === void 0 ? void 0 : element.getAttribute) === null || _a === void 0 ? void 0 : _a.call(element, `data-src`)) !== null && _b !== void 0 ? _b : ``),
+            url: (_d = (_c = element === null || element === void 0 ? void 0 : element.getAttribute) === null || _c === void 0 ? void 0 : _c.call(element, `data-download`)) !== null && _d !== void 0 ? _d : ``,
+        });
+    })) === null || _c === void 0 ? void 0 : _c.reduce) === null || _d === void 0 ? void 0 : _d.call(_c, (all, { title, url }) => {
+        if (!title.length || !url.length)
+            return all;
+        else
+            return all.set(title, url);
+    }, new Map())) !== null && _e !== void 0 ? _e : new Map();
+    const currentImages = (_g = (_f = currentFiles === null || currentFiles === void 0 ? void 0 : currentFiles.reduce) === null || _f === void 0 ? void 0 : _f.call(currentFiles, (all, current) => {
         const ext = path_1.default.extname(current);
         return ![`.png`, `.svg`, `.jpg`, `.jpeg`].includes(ext)
             ? all
             : all.set(current, null);
-    }, new Map())) !== null && _b !== void 0 ? _b : new Map();
-    const ignoredNames = (_f = (_e = (_d = (_c = ignoreList === null || ignoreList === void 0 ? void 0 : ignoreList.split) === null || _c === void 0 ? void 0 : _c.call(ignoreList, `\n`)) === null || _d === void 0 ? void 0 : _d.reduce) === null || _e === void 0 ? void 0 : _e.call(_d, (all, current) => (current ? all.set(current, null) : all), new Map())) !== null && _f !== void 0 ? _f : new Map();
-});
+    }, new Map())) !== null && _g !== void 0 ? _g : new Map();
+    const ignoredNames = (_l = (_k = (_j = (_h = ignoreList === null || ignoreList === void 0 ? void 0 : ignoreList.split) === null || _h === void 0 ? void 0 : _h.call(ignoreList, `\n`)) === null || _j === void 0 ? void 0 : _j.reduce) === null || _k === void 0 ? void 0 : _k.call(_j, (all, current) => (current ? all.set(current, null) : all), new Map())) !== null && _l !== void 0 ? _l : new Map();
+    if (!imageResults.size || !currentImages.size || !ignoredNames.size)
+        return Promise.reject(`${!imageResults.size
+            ? `\nNo images were returned from the endpoint`
+            : ``}${!ignoredNames.size ? `\nThe ignorelist couldn't be parsed` : ``}${!currentImages.size
+            ? `\nThe current dir contents weren't found`
+            : ``}`);
+    imageResults.forEach((_value, key, imageMap) => {
+        if (ignoredNames.has(key) || currentImages.has(key))
+            return imageMap.delete(key);
+        else
+            return;
+    });
+    return Promise.all(Array.from(imageResults, ([title, url]) => lib_1.downloadImage({ title, url })));
+})
+    .catch((err) => console.error(err));
--- wallpaper_play.ts
+++ wallpaper_play.ts
@@ -14,59 +14,67 @@ Promise.all([
   }),
   getIgnoreList(path.resolve(__dirname, `ignore-list.txt`)),
   readdir(path.resolve(__dirname)),
-]).then(
-  ([newResults, ignoreList, currentFiles]: [
-    AxiosResponse<string>,
-    string,
-    string[]
-  ]) => {
-    const resultsHtml = parse(newResults.data);
+])
+  .then(
+    ([newResults, ignoreList, currentFiles]: [
+      AxiosResponse<string>,
+      string,
+      string[]
+    ]) => {
+      const resultsHtml = parse(newResults.data);
 
-    const imageResults = 
-      Array
-        .from(resultsHtml?.querySelectorAll?.(`[data-download][data-src]`) ?? [], element => ({
-          title: nameImage(element?.getAttribute?.(`data-src`) ?? ``),
-          url: element?.getAttribute?.(`data-download`) ?? ``
-        }))
-        ?.reduce?.((all, { title, url }) => {
+      const imageResults =
+        Array.from(
+          resultsHtml?.querySelectorAll?.(`[data-download][data-src]`) ?? [],
+          (element) => ({
+            title: nameImage(element?.getAttribute?.(`data-src`) ?? ``),
+            url: element?.getAttribute?.(`data-download`) ?? ``,
+          })
+        )?.reduce?.((all, { title, url }) => {
           if (!title.length || !url.length) return all;
-          else return all.set(title, url)
+          else return all.set(title, url);
         }, new Map()) ?? new Map();
 
+      const currentImages: Map<string, null> =
+        currentFiles?.reduce?.((all, current) => {
+          const ext = path.extname(current);
+          return ![`.png`, `.svg`, `.jpg`, `.jpeg`].includes(ext)
+            ? all
+            : all.set(current, null);
+        }, new Map()) ?? new Map();
 
-    const currentImages: Map<string, null> =
-      currentFiles?.reduce?.((all, current) => {
-        const ext = path.extname(current);
-        return ![`.png`, `.svg`, `.jpg`, `.jpeg`].includes(ext)
-          ? all
-          : all.set(current, null);
-      }, new Map()) ?? new Map();
+      const ignoredNames: Map<string, null> =
+        ignoreList
+          ?.split?.(`\n`)
+          ?.reduce?.(
+            (all, current) => (current ? all.set(current, null) : all),
+            new Map()
+          ) ?? new Map();
 
-    const ignoredNames: Map<string, null> =
-      ignoreList
-        ?.split?.(`\n`)
-        ?.reduce?.(
-          (all, current) => (current ? all.set(current, null) : all),
-          new Map()
-        ) ?? new Map();
+      if (!imageResults.size || !currentImages.size || !ignoredNames.size)
+        return Promise.reject(
+          `${
+            !imageResults.size
+              ? `\nNo images were returned from the endpoint`
+              : ``
+          }${!ignoredNames.size ? `\nThe ignorelist couldn't be parsed` : ``}${
+            !currentImages.size
+              ? `\nThe current dir contents weren't found`
+              : ``
+          }`
+        );
 
-    if (!imageResults.size || !currentImages.size || !ignoredNames.size)
-      return Promise.reject(`${
-        !imageResults.size
-          ? `\nNo images were returned from the endpoint`
-          : ``
-      }${!ignoredNames.size ? `\nThe ignorelist couldn't be parsed` : ``}${
-        !currentImages.size
-          ? `\nThe current dir contents weren't found`
-          : ``
-      }`);
-    
-    imageResults.forEach((_value, key, imageMap) => {
-      if (ignoredNames.has(key) || currentImages.has(key))
-        return imageMap.delete(key);
-      else return;
-    });
+      imageResults.forEach((_value, key, imageMap) => {
+        if (ignoredNames.has(key) || currentImages.has(key))
+          return imageMap.delete(key);
+        else return;
+      });
 
-    return Promise.all(Array.from(imageResults, ([title, url]) => downloadImage({ title, url })));
-  }
-).catch(err => console.error(err))
+      return Promise.all(
+        Array.from(imageResults, ([title, url]) =>
+          downloadImage({ title, url })
+        )
+      );
+    }
+  )
+  .catch((err) => console.error(err));
