6fc3eee4eb1ebe1eabd3fd991aa843a42e391adb
Patrick McLennan <patrick.a.mclennan@gmail.com>
2021-06-06 15:07:05 -0400

    Static HTML generation + serving and scraping scripts

--- scripts/run_scrapers.sh
+++ scripts/run_scrapers.sh
@@ -4,7 +4,13 @@
   # Run all scrapers
 #
 
+current_path=$(pwd)
+
+cd ~/
+
 node ~/var/services/homes/patrick/Backgrounds/express/dist/scrapers/alphacoders.js
 node ~/var/services/homes/patrick/Backgrounds/express/dist/scrapers/wallhaven.js
 node ~/var/services/homes/patrick/Backgrounds/express/dist/scrapers/wallpaper_play.js
 node ~/var/services/homes/patrick/Backgrounds/express/dist/scrapers/widescreen_wallpaper.js
+
+cd current_path
